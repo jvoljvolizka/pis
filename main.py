28c4d7f629593c1f09b55a773aff4dcc88a593ad
While <ahmetcan1234akcay@gmail.com>
2019-07-02 12:03:03 +0300

    Security update

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
-
 import os
 import json
 import sys
 data=dict()
 path="/home/while/Desktop/git/pis/"
+def filter(text,bad=[";","\\","$","(",")",">","<","\"","'","#","&","*",",","`","?","|","{","}","%","½","^",".."]):
+	c=False
+	for i in bad:
+		if i in text:
+			print("Disabled character",i)
+			sys.exit()
+	return text
 def save():
 	j=json.dumps(data)
 	f=open(path+"settings/data.json","w")
 	f.write(j)
 	f.close()
 if  not os.path.exists(path+"settings/data.json"):
-	os.mkdir("settings")
+	try:
+		os.mkdir("settings")
+	except:
+		pass
 	print("Pleas create user for shh connection !")
-	name=input("Connection name = ")
-	uname=input("Username = ")
-	ip = input("IP = ")
-	port=input("Port = ")
-	location=input("Default transfer location = ")
+	name=filter(input("Connection name = "))
+	uname=filter(input("Username = "))
+	ip = filter(input("IP = "))
+	port=filter(input("Port = "))
+	location=filter(input("Default transfer location = "))
 	print("Username = ",uname)
 	print("IP = ",ip)
-	
 	print("Completed !")
 	data[name]=dict()
 	data[name]['ip']=ip
@@ -40,12 +48,13 @@ if len(sys.argv)>1:
 		print("\t-f <connection name> <transfer file> <transfer location optional> file transfer")
 	elif "--add" in sys.argv:
 		i=sys.argv[2].split(":")
+		filiter(i[0])
 		data[i[0]]=dict()
-		data[i[0]]['ip']=i[1]
-		data[i[0]]['port']=i[2]
-		data[i[0]]['uname']=i[3]
+		data[i[0]]['uname']=filter(i[1])
+		data[i[0]]['ip']=filter(i[2])
+		data[i[0]]['port']=filter(i[3])
 		if len(i)>4:
-			data[i[0]]['location']=i[4]
+			data[i[0]]['location']=filter(i[4])
 		else:
 			data[i[0]]['location']="/tmp/"
 		save()
@@ -53,8 +62,10 @@ if len(sys.argv)>1:
 		print("dosya aktarımı şeysi")
 	else:
 		try:
-			print(sys.argv[1])
 			if not sys.argv[1] in data:
 				print("User not exists")
+			else:
+				n=filter(sys.argv[1])
+				os.system(f"ssh {filter(data[n]['uname'])}@{filter(data[n]['ip'])} -p {filter(data[n]['port'])}")
 		except:
 			print("Pleas enter connection name !")
