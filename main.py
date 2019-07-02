f9ffe9bbcc1784839b41eb0be79b34de53527eba
While <ahmetcan1234akcay@gmail.com>
2019-07-02 11:10:58 +0300

    UPDATE

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import sys
 data=dict()
 if  not os.path.exists("settings/data.json"):
 	os.mkdir("settings")
@@ -9,6 +10,7 @@ if  not os.path.exists("settings/data.json"):
 	name=input("Connection name = ")
 	uname=input("Username = ")
 	ip = input("IP = ")
+	port=input("Port = ")
 	location=input("Default transfer location = ")
 	print("Username = ",uname)
 	print("IP = ",ip)
@@ -16,6 +18,7 @@ if  not os.path.exists("settings/data.json"):
 	print("Completed !")
 	data[name]=dict()
 	data[name]['ip']=ip
+	data[name]['port']=port
 	data[name]['uname']=uname
 	data[name]['location']=location
 	j=json.dumps(data)
@@ -26,4 +29,22 @@ if data==dict():
 	f=open("settings/data.json")
 	data=json.loads(f.read())
 	f.close()
-print(data)
+if len(sys.argv)>1:
+	if "--help" in sys.argv or "-h" in sys.argv:
+		print("pis")
+		print("-c -n <connection name> ssh connection")
+		print("-c --name <connection name> ssh connection")
+		print("--add <name>:<username>:<ip>:port no spaces")
+		print("-f <connection name> <transfer file> <transfer location optional> file transfer")
+	elif "-c" in sys.argv:
+		if "-n" in sys.argv or "--name" in sys.argv:
+			print(sys.argv[3])
+	elif "--add" in sys.argv:
+		i=sys.argv[2].split(":")
+		data[i[0]]=dict()
+		data[i[0]]['ip']=i[1]
+		data[i[0]]['port']=i[2]
+		data[i[0]]['uname']=i[3]
+		data[i[0]]['location']=i[4]
+	elif "-f" in sys.argv:
+		print("dosya aktarımı şeysi")
